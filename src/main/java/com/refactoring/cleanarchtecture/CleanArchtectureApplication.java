47f496e00c9233c0017d30705ed720d64746c884
himina1227 <himina1227@gmail.com>
2023-03-20 20:20:46 +0900

    - init commit

---
+++ src/main/java/com/refactoring/cleanarchtecture/CleanArchtectureApplication.java
@@ -0,0 +1,13 @@
+package com.refactoring.cleanarchtecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchtectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchtectureApplication.class, args);
+    }
+
+}
